ce43974bbe1992c157bf63d8f91a619cf6597313
koji <torikou3685@gmail.com>
2020-02-16 23:17:44 +0900

    Initial Commit

---
+++ CameraSample_SwiftUITests/CameraSample_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  CameraSample_SwiftUITests.swift
+//  CameraSample_SwiftUITests
+//
+//  Created by koji torishima on 2020/02/16.
+//  Copyright © 2020 koji torishima. All rights reserved.
+//
+
+import XCTest
+@testable import CameraSample_SwiftUI
+
+class CameraSample_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
